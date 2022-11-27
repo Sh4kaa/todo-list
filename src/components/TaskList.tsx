7fcc55e74eb2445ddb8ae22d3c039ae49e6ad298
Dario Souza <darioo_dario@hotmail.com>
2022-11-27 14:35:32 -0300

    exibindo items do todo

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import { ITask } from '../interfaces/Task';
+//css
+import styles from './TaskList.module.css';
 
-type Props = {};
+interface Props {
+  taskList: ITask[];
+}
 
-const TaskList = (props: Props) => {
+const TaskList = ({ taskList }: Props) => {
   return (
-    <div>
-      <p>Lista de tarefas</p>
-    </div>
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id}>
+            <div>
+              <h4>{task.title}</h4>
+              <p>{task.difficulty}</p>
+            </div>
+            <div>
+              <i className="bi bi-pencil"></i>
+              <i className="bi bi-trash"></i>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas</p>
+      )}
+    </>
   );
 };
 

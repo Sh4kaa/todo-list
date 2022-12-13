9f6970c9ff8ae4efec8d7824c3cb2595b544658e
Dario Souza <darioo_dario@hotmail.com>
2022-12-12 22:46:52 -0300

    alterações finais do projeto

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <p>
-        <span>React + TS Todo</span>@2022
+        <span>React + TS Todo | By Dario</span>@2022
       </p>
     </footer>
   );
